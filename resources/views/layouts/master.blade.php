7adcf8a4d05b523ddcbc16f49b628e70faaba879
Aleksandar <aleksandarkurta1988@gmail.com>
2019-03-03 19:29:55 +0100

    Statistics page created with view and method in HomeController

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -47,7 +47,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+      <a href="{{ route('home') }}" class="brand-link">
       <img src="{{ asset('img/logo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">First Laravel Blog</span>
@@ -87,6 +87,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="{{ route('statistics') }}" class="nav-link">
+              <i class="nav-icon fa fa-th"></i>
+              <p>
+                Statistics
+              </p>
+            </a>
+          </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
@@ -97,7 +105,26 @@ scratch. This page gets rid of all links and provides the needed markup only.
  
   <div class="content-wrapper">
 
-    
+     <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Dashboard</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Dashboard v2</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+  @yield('content')
+
   </div>
   <!-- /.content-wrapper -->
 
